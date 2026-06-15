56903c534c7668784100a128b1ba1a27da8c6aa6
banglar-itihas <36944649+banglar-itihas@users.noreply.github.com>
2026-06-15 18:59:40 +0600

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,1 +1,15 @@
+const GA_MEASUREMENT_ID = "G-LM22QLMN2Q";
 
+const script = document.createElement('script');
+script.async = true;
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', GA_MEASUREMENT_ID);
